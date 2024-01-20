1d820ae33d7c100c3513c5fb10676764e2549b64
matevos-g <matevos.g@tumo.org>
2024-01-20 14:12:58 +0400

    Change Consol Log

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", function (req, res) {
 app.post("/addInfo", function (req, res) {
     const Name = req.body.Name
     const Password = req.body.Password
-    console.log(Name, Password);
+    console.log(Name, Password, "ME");
 })
 app.get("/*", function (req, res) {
     res.send("<h1>The Page You Are Looking For Is Unavailable (Error 404)</h1>")
