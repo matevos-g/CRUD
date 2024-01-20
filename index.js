97c88b6832eca8958704f5b6a9a5eace440574c1
matevos-g <matevos.g@tumo.org>
2024-01-20 15:28:07 +0400

    connected MongoDB And Mongoose

--- index.js
+++ index.js
@@ -2,22 +2,42 @@ var express = require("express")
 var bodyParser = require("body-parser")
 var path = require("path")
 var app = express()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'))
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, './public/form.html'))
-})
-app.post("/addInfo", function (req, res) {
-    const Name = req.body.Name
-    const Password = req.body.Password
-    console.log(Name, Password, "ME");
-})
-app.get("/*", function (req, res) {
-    res.send("<h1>The Page You Are Looking For Is Unavailable (Error 404)</h1>")
-})
+const mongoose = require('mongoose');
+const { stringify } = require("querystring");
+const { log } = require("console");
+const connectionString = 'mongodb+srv://matevos-g:Matos1984@crud.ydeqrqw.mongodb.net/sample_mflix';
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', async () => {
+    console.log('Connected to MongoDB!');
 
-app.listen(3000, function () {
-    console.log("Running In 3000");
-})
+    try {
+        const allMovies = await mongoose.connection.db.collection('movies').find().toArray(); // .insertMany(newMovies);
+
+        console.log('All Movies:', allMovies);
+    } catch (error) {
+        console.error('Error retrieving movies:', error);
+    } finally {
+        mongoose.connection.close();
+    }
+}) 
+
+
+
+// app.get("/", function (req, res) {
+//     res.sendFile(path.join(__dirname, './public/form.html'))
+// })``
+// app.post("/addInfo", function (req, res) {
+//     const Name = req.body.Name
+//     const Password = req.body.Password
+//     console.log(Name, Password, "ME");
+// })
+// app.get("/*", function (req, res) {
+//     res.send("<h1>The Page You Are Looking For Is Unavailable (Error 404)</h1>")
+// })
+
+// app.listen(3000, function () {
+//     console.log("Running In 3000");
+// })
